e8fa398c3ba6a2717d63f7b35bd7d6e236d95afa
chizzora <opticalzaxar2001@gmail.com>
2022-08-08 16:41:41 +0300

    feat: add test solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let h = String(n);
+  let result = '';
+  if (n < 0) {
+    h = String(-n);
+  }
+    for (let i = 0; i < h.length; i++) {
+        result = `${h[i]}${result}`
+    }
+    return Number(result);
 }
